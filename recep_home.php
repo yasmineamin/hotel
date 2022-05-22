58e65585f2a7e8d26cae88fccaf9fc9682a654a7
Habiba93 <97574320+Habiba93@users.noreply.github.com>
2022-05-22 16:15:52 -0700

    Update and rename Home.php to recep_home.php

---
+++ recep_home.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<!--<link rel="stylesheet" type="text/CSS"  href="stylesheet.css" />-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
+.0/css/font-awesome.min.css">
+</head>
+<?php
+session_start();
+include "recep_menu.php";?>
+<head>
+<title>Main | Receptionist view</title>
+</head>
+<body>
+
+<h2>Welcome Back!</h2>
+
+
+
+
+
+
+
+</body>
+</html>
