2cd47bc82a302c0d5859a6518d11d25deb9af6b2
Habiba93 <97574320+Habiba93@users.noreply.github.com>
2022-05-22 16:15:14 -0700

    Update and rename Add.php to recep_add.php

--- recep_add.php
+++ recep_add.php
@@ -1,21 +1,39 @@
 <html>
+
+<head>
+<!--<link rel="stylesheet" type="text/CSS"  href="stylesheet.css" />-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
+.0/css/font-awesome.min.css">
+
 <?php
 session_start();
-include "menu.php";?>
-<head>
-<title>Add item</title>
+include "recep_menu.php";?>
+<title>Add User</title>
+<style>
+body {
+	  margin: 0;
+	  background: #f2f2f2;
+	  width: 100%;
+    }
+	div.input-group{
+	padding:10px 20px;
+	font-size: 1rem;
+	}
+	form{
+	margin-top:20px;
+	}
+</style>
 </head>
 <body>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "receptionist2";
+$servername="localhost";
+$username="root";
+$password="";
+$DB="login";
 
-
-$conn = new mysqli($servername, $username, $password, $dbname,3306)or die("not connected");
-    echo "connected";
-
+$conn = mysqli_connect($servername,$username,$password,$DB);
 $emailError="";
 
 if(isset($_POST['Submit'])){  
@@ -41,7 +59,7 @@ if (filter_var($_POST["Age"], FILTER_VALIDATE_INT) ==true) {
  
         
 		$sql="insert into user(First Name,Email,Password,Age) values(".$_POST['Name']."','".$_POST["Email"]."','".$_POST["Password"]."',,'".$_POST["Age"]."')";
-	 	$result=mysqli_query($conn,$sql);
+	 	$result=mysqli_query($conn,$sql)or die( mysqli_error($conn));
 		 
  
 }
@@ -49,17 +67,28 @@ if (filter_var($_POST["Age"], FILTER_VALIDATE_INT) ==true) {
 
  
 <form action="" method="post">
+<div class="input-group">
   Name:<br>
+  
   <input type="text" name="Name"><br> 
+  </div>
+  <div class="input-group">
   Email:<br>
   <input type="text" name="Email"> <br> 
+  </div>
+  <div class="input-group">
   Password:<br>
   <input type="Password" name="Password"><br>
+  </div>
+  <div class="input-group">
   Age:<br>
   <input type="Password" name="Age"><br>
+  </div>
+  <div class="input-group">
   <input type="submit" value="Add" name="Add">
- 
+ </div>
 </form>
+
 
 
 
