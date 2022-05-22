a5318bf07f19d0397a84eacce825a09e1e5f1430
Habiba93 <97574320+Habiba93@users.noreply.github.com>
2022-05-22 16:20:11 -0700

    Update recep_home.php

--- recep_home.php
+++ recep_home.php
@@ -5,13 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
 .0/css/font-awesome.min.css">
-</head>
-<?php
+    <title>Main | Receptionist view</title>
+    <style>
+        body{
+background: #f2f2f2;	
+}
+    </style>
+    <?php
 session_start();
 include "recep_menu.php";?>
-<head>
-<title>Main | Receptionist view</title>
 </head>
+    
+
 <body>
 
 <h2>Welcome Back!</h2>
