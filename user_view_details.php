2a9c772de031d231c1db30c53ef37c37ab067057
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2022-05-24 11:53:18 -0700

    Update user_view_details.php

--- user_view_details.php
+++ user_view_details.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!--<link rel="stylesheet" type="text/CSS"  href="stylesheet.css" />-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
+.0/css/font-awesome.min.css">
 	<title>User Signed In</title>
+	<?php
+
+include "user_menu.php";
+?>
 </head>
 <style>
 	body {
@@ -13,50 +22,6 @@
 	}
 	td {
 		padding: 15px;
-	}
-	#td1
-	{
-		background-color: rgba(09,41,98,0.9);
-		color: white;
-		border: 10px;
-		margin-top: -10px;
-		padding: 10px;
-		text-align: center;
-	}
-	ul {
-	  	margin: 0;
-	  	padding: 0;
-	  	width: 22%;
-	  	font-size: 24px;
-	  	background-color: rgba(09,41,98,0.9);
-	  	text-decoration: none;
-	  	position: fixed;
-	  	height: 100%;
-	  	overflow: auto;
-	}
-	li {
-		color: white;
-	}
-	li a {
-	  	display: block;
-	  	color: white;
-	  	padding: 8px 16px;
-	  	text-decoration: none;
-	}
-	li a:visited {
-	  	background-color: #e6b800;
-	  	color: white;
-	  	text-decoration: underline;	
-	}
-	li a:active {
-	  	background-color: #e6b800;
-	  	color: white;
-	  	text-decoration: underline;		
-	}
-	li a:hover {
-	  	background-color: #e6b800;
-	  	color: white;
-	  	text-decoration: underline;
 	}
 	.basic_box {
 		border: 1px solid #ccc;
@@ -86,21 +51,8 @@ include_once("functions.php");
 		$result=mysqli_query($conn, $sql)or die( mysqli_error($conn));
 		$row=mysqli_fetch_row($result);
 		?>
-	<table style="width: 100%;">
-		<tr>
-			<td id="td1" style="padding: 10px; font-size: 48px; position: sticky; z-index: 1">THE <p style="color: #e6b800; display: inline;">DELUXE</p> HOTEL</td>
-			<td id="td1" style="font-size: 25px; text-align: right;">Hello, <?php echo $row[2]; ?></td>
-		</tr>
-	</table>
-	<ul>
-		<li><a href="user_view.php">My Info</a></li>
-		<li><a href="bookroom.php">Book A Room</a></li>
-		<li><a href="user_room_status.php">Show Booking Status</a></li>
-		<li><a href="user_payment.php">Payment</a></li>
-		<li><a href="user_booking_history.php">Booking History</a></li>
-		<li><a href="index.php">Logout</a></li>
-	</ul>
-	<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+		
+	<div style="margin-left:2%;padding:1px 16px;height:1000px;">
 		<p style="margin-left: 10%; margin-top: 5%; font-size: 24px;"></p>
 			<table  class="basic_box decor">
 				<tr>
