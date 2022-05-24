6d766cc3c7dcdc6e0a57ed6431f1f9479dd41dcc
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2022-05-24 11:54:57 -0700

    Update user_room_history.php

--- user_room_history.php
+++ user_room_history.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 	<title>User Room Status</title>
+	<?php
+
+include "user_menu.php";
+?>
 </head>
 <style>
 	body {
@@ -86,33 +90,10 @@ include_once("functions.php");
 		$row=mysqli_fetch_row($result);
 		
 		?>
-	<table style="width: 100%;">
-		<tr >
-			<td id="td1" style="padding: 10px; font-size: 48px;	position: fixed;">THE <p style="color: #e6b800; display: inline;">DELUXE</p> HOTEL</td>
-			<td id="td1" style="font-size: 25px; text-align: right;	position: fixed;">Hello, <?php echo $row[2]; ?></td>
-		</tr>
-	</table>
-	<ul>
-		<li><a href="user_view.php">My Info</a></li>
-		<li><a href="bookroom.php">Book A Room</a></li>
-		<li><a href="user_room_status.php">Show Booking Status</a></li>
-		<li><a href="user_payment.php">Payment</a></li>
-		<li><a href="user_booking_history.php">Booking History</a></li>
-		<li><a href="index.php">Logout</a></li>
-	</ul>
+	
 	<div style="margin-left:25%;padding:1px 16px;height:1000px;">
 		<p style="margin-left: 10%; margin-top: 5%; font-size: 28px;"></p>
-				<?php
-			$conn = mysqli_connect($servername,$username,$password,$DB);
-			if($conn->connect_error)
-			{
-				die("Connection failed: ".$conn->connect_error);
-			}
-			$bid = $_POST["book_id"];
-			$sql = "SELECT * FROM booked_hist WHERE book_id='$bid'";
-			$result=mysqli_query($conn,$sql);
-			$row=mysqli_fetch_row($result);
-		?>
+				
 			<table class="basic_box">
 				<tr>
 					<th colspan="2">Booking History</th>
