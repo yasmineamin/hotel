24e3793735cf0f545ac4801309b62e65db787517
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2022-05-24 21:24:51 +0200

    Add files via upload

---
+++ user_payment.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Payment</title>
+	<?php
+
+include "user_menu.php";
+?>
+
+<style>
+	body {
+	  margin: 0;
+	  background: #f2f2f2;
+	}
+	
+	.basic_box {
+		border: 1px solid #ccc;
+		border-radius: 15px;
+		margin: auto;
+		width: 600px;
+		padding: 50px;
+		box-shadow: 0 10px 20px rgba(0,0,0,0.19);
+	}
+
+</style>
+</head>
+
+<body>
+	<?php
+		session_start();
+
+
+		include_once("saveconnection.php");
+		include_once("functions.php");
+		$conn = new mysqli($host,$username,$pass,$DB);
+		if($conn->connect_error)
+		{
+			die("Connection failed: ".$conn->connect_error);
+		}
+		$sql = "SELECT * from temp_session";
+		$result=mysqli_query($conn, $sql)or die( mysqli_error($conn));
+		$row=mysqli_fetch_row($result); 
+		?>
+	
+	<div style="margin-left:1%;padding:1px 16px;height:1000px;">
+		<p style="margin-left: 1%; margin-top: 5%; font-size: 28px;"></p>
+			<div class="basic_box"><table>
+				<tr>
+					<td colspan="6"><p style="font-size: 28px; text-align: center; text-decoration: underline;"><b>Payment</b></p>
+				</td>
+				<tr>
+					<th>Booking ID</th>
+					<th>Name</th>
+					<th>Room Type</th>
+					<th>Check-in Date</th>
+					<th>Check-out Date</th>
+					<th>Price</th>
+				</tr>
+				<tr>
+				<?php
+					$conn = new mysqli($host,$username,$pass,$DB);
+		if($conn->connect_error)
+		{
+			die("Connection failed: ".$conn->connect_error);
+		}
+					$sql1 = "SELECT * from confirmed_booking";
+					if ($result=mysqli_query($conn,$sql1))
+				  	{
+				  		while ($row=mysqli_fetch_row($result)or die( mysqli_error($conn)))
+				    	{
+				    		?>
+				    		<td><?php echo $row[14]; ?></td>
+				   			<td><?php echo $row[1]; ?></td>
+				   			<td><?php echo $row[3]; ?></td>
+				   			<td><?php echo $row[4]; ?></td>
+				    		<td><?php echo $row[5]; ?></td>
+				    		<td><?php echo $row[13]; ?></td>
+				</tr><?php
+				    	}
+				    	mysqli_free_result($result); 
+				    }?>
+				</table><br><br>
+				<table>
+				<tr>
+					<td colspan="1">Enter Booking ID:</td>
+					<td colspan="2">
+						<form action="payment.php" method="post">
+							<input type="number" name="book_id">
+					</td>
+					<td style="text-align: center;"><button type="submit">Pay Now</button></td>
+				</tr>
+				
+		</table><br></div>
+	</div>
+</body>
+</html>
