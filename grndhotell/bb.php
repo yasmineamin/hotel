2238919d533940082e2260e9299d443d1a134a6a
hudaamr <94346307+hudaamr@users.noreply.github.com>
2022-05-28 15:24:33 +0200

    Add files via upload

---
+++ grndhotell/bb.php
@@ -0,0 +1,68 @@
+<nav class="navbar navbar-expand-lg navbar-dark px-0 py-3 position-absolute top-0 start-0 w-full bg-dark bg-lg-transparent">
+    <div class="container-xl max-w-screen-xl">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            <img src="https://preview.webpixels.io/web/img/logos/clever-light.svg" class="h-10" alt="...">
+        </a>
+        <!-- Navbar toggle -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <!-- Nav -->
+            <ul class="navbar-nav mx-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+            <!-- Right navigation -->
+            <div class="navbar-nav ms-lg-4">
+                <a class="nav-item nav-link" href="#">Sign in</a>
+            </div>
+            <!-- Action -->
+            <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
+                <a href="#" class="btn btn-sm btn-neutral w-full w-lg-auto">
+                    Register
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
+<section class="position-relative pt-48 pb-40 bg-dark bg-cover bg-size--cover" style="background-image:url(https://images.unsplash.com/photo-1549082984-1323b94df9a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)">
+    <!-- Overlay -->
+    <span class="position-absolute top-0 start-0 w-full h-full bg-dark opacity-80"></span>
+    <!-- Content -->
+    <div class="container-lg max-w-screen-xl position-relative overlap-10 text-center text-lg-start pt-5 pb-5 pt-lg-6">
+        <div class="row row-grid align-items-center">
+            <div class="col-lg-8 text-center text-lg-start">
+                <!-- Title -->
+                <h1 class="ls-tight font-bolder display-5 text-white mb-5">
+                    We built incredible web products for designers & developers
+                </h1>
+                <!-- Text -->
+                <p class="lead text-white text-opacity-80 mb-10 w-lg-2/3">
+                    For over 5 years, we pride ourselves on our commitment to excellence, as well as our ability to deliver for our customers.
+                    </h1>
+                    <!-- Buttons -->
+                <div class="mt-10 mx-n2">
+                    <a href="#" class="btn btn-lg btn-primary shadow-sm mx-2 px-lg-8">
+                        Get started
+                    </a>
+                    <a href="#" class="btn btn-lg btn-neutral mx-2 px-lg-8">
+                        Learn more
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
