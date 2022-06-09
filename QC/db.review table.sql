cc64aca07b626150155da1a936474e835dc8e9c7
nourjebreel <105165311+nourjebreel@users.noreply.github.com>
2022-06-09 23:20:08 +0200

    Add files via upload

---
+++ QC/db.review table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `review_table` (
+  `review_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int(1) NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `review_table`
+--
+
+INSERT INTO `review_table` (`review_id`, `user_name`, `user_rating`, `user_review`, `datetime`) VALUES
+(6, 'John', 4, 'Nice Product, Value for money', 1621935691),
+(7, 'Peter', 5, 'Nice Product with Good Feature.', 1621939888);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `review_table`
+--
+ALTER TABLE `review_table`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `review_table`
+--
+ALTER TABLE `review_table`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
