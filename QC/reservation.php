864d90704fc7fe60bd02aafeb185d83fa266d426
hudaamr <94346307+hudaamr@users.noreply.github.com>
2022-06-10 19:55:02 +0200

    Add files via upload

---
+++ QC/reservation.php
@@ -0,0 +1,35 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>QC</title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/stylee.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+	<!---addtional-->
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+ <?php
+session_start();
+include "qc_menu.php";
+include("dp.php");
+include("../functions.php");?>
+</head>
+
+<body>
+
+  
+
+
+</body>
+
+</html>
