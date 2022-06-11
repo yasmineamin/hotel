d44c27a60b0ba6c77258f33fc46d254d967cbea8
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2022-06-11 19:43:24 +0200

    Add files via upload

---
+++ USER/confirm_room2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Room Confirm</title>
+		<?php
+
+include "user_menu.php";
+?>
+<style>
+	body {
+	  margin: 0;
+	  background: #f2f2f2;
+	}
+	table {
+		font-size: 22px;
+	}
+	#td1
+	{
+		background-color: rgba(09,41,98,0.9);
+		color: white;
+		border: 10px;
+		margin-top: -10px;
+		padding: 10px;
+		text-align: center;
+	}
+	td {
+		text-align: left;
+	}
+	th {
+		font-weight: bold;
+		text-align: left;
+	}
+	ul {
+	  	list-style-type: none;
+	  	margin: 0;
+	  	padding: 0;
+	  	width: 22%;
+	  	font-size: 24px;
+	  	background-color: rgba(09,41,98,0.9);
+	  	text-decoration: none;
+	  	position: fixed;
+	  	height: 100%;
+	  	overflow: auto;
+	}
+	li {
+		color: white;
+	}
+	li a {
+	  	display: block;
+	  	color: white;
+	  	padding: 8px 16px;
+	  	text-decoration: none;
+	}
+	li a:active {
+	  	background-color: #e6b800;
+	  	color: white;
+	  	text-decoration: underline;		
+	}
+	li a:hover {
+	  	background-color: #e6b800;
+	  	color: white;
+	  	text-decoration: underline;
+	}
+	.basic_box {
+		border: 1px solid #ccc;
+		border-radius: 15px;
+		margin: auto;
+		width: 600px;
+		padding: 50px;
+		box-shadow: 0 10px 20px rgba(0,0,0,0.19);
+	}
+	
+</style>
+</head>
+
+<body>
+	<?php
+		$conn = new mysqli("localhost","root","", "login");
+		if($conn->connect_error)
+		{
+			die("Connection failed: ".$conn->connect_error);
+		}
+		$sql = "SELECT * from temp_session";
+		$result=mysqli_query($conn, $sql);
+		$row=mysqli_fetch_row($result);
+		?>
+	
+	<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+		<p>Sorry! All rooms are full in this type.<br>Wait for the rooms to get empty.</p>
+		<p> OR Check other rooms</p> 
+		<P><br>Click Next to get redirected to rooms gallery.</P>
+		<a href="user_room.php" style="width: 10%; text-align: center; color: black; border: 3px solid #e6b800;">Next</a>
+	</div>
+</body>
+</html>
