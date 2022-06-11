87030cd9c10cde6e153ebcd891fbdb421f2895ca
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2022-06-11 14:06:24 -0700

    Update user_home.php

--- USER/user_home.php
+++ USER/user_home.php
@@ -37,51 +37,9 @@ include "user_menu.php";
 	}
 </style>
 <body>
-	<?php
-	session_start();
+	<h2 style="align-text:center;"><i>Welcome back</i></h2>
+	<p style="align-text:center;">We are happy to see you</p>
 
-
-include_once("saveconnection.php");
-include_once("functions.php");
-		$conn = new mysqli($host,$username,$pass,$DB);
-		if($conn->connect_error)
-		{
-			die("Connection failed: ".$conn->connect_error);
-		}
-		$sql = "SELECT * from temp_session";
-		$result=mysqli_query($conn, $sql)or die( mysqli_error($conn));
-		$row=mysqli_fetch_row($result);
-		?>
-		
-	<div style="margin-left:2%;padding:1px 16px;height:1000px;">
-		<p style="margin-left: 10%; margin-top: 5%; font-size: 24px;"></p>
-			<table  class="basic_box decor">
-				<tr>
-					<td colspan="2"><p style="font-size: 38px; text-align: center;"><b>Welcome!</b></p>
-				</td>
-				<tr>
-					<td><b>Name: </b></td>
-					<td><?php echo $row[2] ?><br></td>
-				</tr>
-				<tr>
-					<td><b>Phone number: </b></td>
-					<td><?php echo $row[0] ?><br></td>
-				</tr>
-				<tr>
-					<td><b>Email address: </b></td>
-					<td><?php echo $row[3] ?><br></td>
-				</tr>
-				<tr>
-					<td><b>Date of birth: </b></td>
-					<td><?php echo $row[5] ?><br></td>
-				</tr>
-				<tr>
-					<td><b>ID Proof: </b></td>
-					<td><?php echo $row[4] ?><br></td>
-				</tr>
-				<tr><td></td></tr><tr><td></td></tr>
-			</table>
-	</div>
 </body>
 </html>
     
